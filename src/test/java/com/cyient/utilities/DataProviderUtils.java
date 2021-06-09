e7ef497620b61e3bf2f11bf9233fbb3ea91071e8
gs59153 <gs59153@MKD-CORP-BCP081.CORP.CYIENT.COM>
2021-06-09 12:25:58 +0530

    Data Driven Activity

--- src/test/java/com/cyient/utilities/DataProviderUtils.java
+++ src/test/java/com/cyient/utilities/DataProviderUtils.java
@@ -1,52 +1,18 @@
 package com.cyient.utilities;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.DataProvider;
 
 public class DataProviderUtils {
 	
-	/*
-	 * FileInputStream file = new
-	 * FileInputStream("src/test/resources/testdata/Book1.xlsx");
-	 * 
-	 * XSSFWorkbook book = new XSSFWorkbook(file); XSSFSheet sheet =
-	 * book.getSheet("validCredentialTest");
-	 * 
-	 * Object [][] main = new Object[3][4];
-	 * 
-	 * for(int i=0;i<3;i++) { for(int j=0;j<4;j++) { XSSFRow row = sheet.getRow(i);
-	 * XSSFCell cell = row.getCell(j); DataFormatter format = new DataFormatter();
-	 * 
-	 * main[i][j] = format.formatCellValue(cell); } System.out.println(); } }
-	 */
-	
 	
 	@DataProvider
-	public Object[][] validCredentialData() throws IOException{
+	public Object[][] validCredentialData(){
 		
-		FileInputStream file = new FileInputStream("src/test/resources/testdata/Book1.xlsx");
-		XSSFWorkbook book = new XSSFWorkbook(file);
-		XSSFSheet sheet = book.getSheet("validCredentialTest");
-		Object[][] main = new Object[2][4];
+		Object[][] main = new Object[3][4];
 		
-		for(int i=1;i<3;i++) {
-			for(int j=0;j<4;j++) {
-				XSSFRow row = sheet.getRow(i);
-				XSSFCell cell = row.getCell(j);
-				DataFormatter format = new DataFormatter();
-				
-				main[i-1][j] = format.formatCellValue(cell);
-			}
-		}
-		
-	/*	main[0][0]="admin";
+		main[0][0]="admin";
 		main[0][1]="pass";
 		main[0][2]="English (Indian)";
 		main[0][3]="OpenEMR";
@@ -59,7 +25,7 @@ public class DataProviderUtils {
 		main[2][0]="accountant";
 		main[2][1]="accountant";
 		main[2][2]="English (Indian)";
-		main[2][3]="OpenEMR";*/
+		main[2][3]="OpenEMR";
 		
 		return main;
 	}
